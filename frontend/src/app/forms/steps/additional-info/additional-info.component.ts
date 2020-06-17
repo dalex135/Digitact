dc9eb893d4daedbd51f869a113ebbcef9f13f43c
vishwasanavatti <vishwasanavatti@gmail.com>
2020-06-17 18:28:08 +0200

    page is structured and data persistance

--- frontend/src/app/forms/steps/additional-info/additional-info.component.ts
+++ frontend/src/app/forms/steps/additional-info/additional-info.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * @description
+ *  This component renders the additional information given by Applicant
+ */
+import { Component, Input } from '@angular/core';
 
+import { FormGroup } from '../../../common/forms/forms';
+import { FormsData } from '../../../model/forms-data.model';
 @Component({
-  selector: 'app-additional-info',
+  selector: 'form-additional-info',
   templateUrl: './additional-info.component.html',
   styleUrls: ['./additional-info.component.scss'],
 })
-export class AdditionalInfoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class AdditionalInfoComponent {
+  @Input()
+  formsData: FormGroup<FormsData>;
 }
